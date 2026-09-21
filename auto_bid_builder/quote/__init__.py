684982b32feec28dbca9642a2d8708c3e6f72e9f
Chase Lunsford <39634453+CaMaLabs@users.noreply.github.com>
2026-09-21 06:45:53 -0700

    Add quote package

---
+++ auto_bid_builder/quote/__init__.py
@@ -0,0 +1,5 @@
+"""JTI-style quotation parsing and quote/scope helpers."""
+
+from auto_bid_builder.quote.jti import QuoteDocument, QuoteHeader, QuoteLine, parse_jti_quote_text
+
+__all__ = ["QuoteDocument", "QuoteHeader", "QuoteLine", "parse_jti_quote_text"]
