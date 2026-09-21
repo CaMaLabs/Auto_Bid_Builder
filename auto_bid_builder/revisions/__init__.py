b5b7c3195f79a50f5fce16d4c03442d5dbfe9845
Chase Lunsford <39634453+CaMaLabs@users.noreply.github.com>
2026-09-21 06:46:37 -0700

    Add revisions package

---
+++ auto_bid_builder/revisions/__init__.py
@@ -0,0 +1,5 @@
+"""Revision and bulletin impact detection."""
+
+from auto_bid_builder.revisions.impact import RevisionImpact, diff_revision_signals, extract_revision_signals
+
+__all__ = ["RevisionImpact", "diff_revision_signals", "extract_revision_signals"]
