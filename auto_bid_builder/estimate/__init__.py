4e3b9673704e6c2549013e093fa2847ad73d105b
Chase Lunsford <39634453+CaMaLabs@users.noreply.github.com>
2026-09-21 18:16:46 -0700

    Expose guided estimate APIs

--- auto_bid_builder/estimate/__init__.py
+++ auto_bid_builder/estimate/__init__.py
@@ -1,5 +1,17 @@
-"""Historical estimating and pricing-calibration helpers."""
+"""Historical estimating, pricing calibration, and guided estimate helpers."""
 
 from .cost_detail import CostDetailDocument, CostDetailLine, parse_cost_detail_pdf, parse_cost_detail_text
+from .draft import EstimateDraft, EstimateLine, create_estimate_from_workspace, load_estimate, save_estimate, write_quote_preview
 
-__all__ = ["CostDetailDocument", "CostDetailLine", "parse_cost_detail_pdf", "parse_cost_detail_text"]
+__all__ = [
+    "CostDetailDocument",
+    "CostDetailLine",
+    "parse_cost_detail_pdf",
+    "parse_cost_detail_text",
+    "EstimateDraft",
+    "EstimateLine",
+    "create_estimate_from_workspace",
+    "load_estimate",
+    "save_estimate",
+    "write_quote_preview",
+]
